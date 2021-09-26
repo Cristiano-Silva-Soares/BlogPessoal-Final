068437dcaadcf5d3353a7ae449be766fd264bb01
Cristiano-Silva-Soares <criztianosoares99@outlook.com>
2021-09-26 17:29:38 -0300

    [Feature Cristiano - Task:Implementação das pages de cadastro,login, e landing page do meu blog]

--- Front_MeuBlog/src/app/app.module.ts
+++ Front_MeuBlog/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
